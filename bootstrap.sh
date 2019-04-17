1a304ac1516c4ceca51e08d87e67f4751a46a451
Cody Krainock <codykrainock@gmail.com>
2019-04-16 23:21:49 -0700

    Update gitignore and bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "themes/" \
     --exclude ".gitmodules" \
+    --exclude ".gitignore" \
     -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
